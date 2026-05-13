ce54a7efb2186f42c4112e0dabf3abd15b34f3a1
absoluterrrr <obuhovmisa814@gmail.com>
2026-05-13 15:46:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,3 @@
-import os
-
-# API keys (optional)
-SHODAN_API_KEY = os.getenv("SHODAN_API_KEY")
-VIRUSTOTAL_API_KEY = os.getenv("VIRUSTOTAL_API_KEY")
-IPINFO_TOKEN = os.getenv("IPINFO_TOKEN")
-
-# Network and concurrency
-DEFAULT_TIMEOUT = int(os.getenv("OSINT_TIMEOUT", "10"))
-MAX_THREADS = int(os.getenv("OSINT_THREADS", "20"))
-
-# Safety
-BATCH_SCAN_THRESHOLD = int(os.getenv("OSINT_BATCH_SCAN_THRESHOLD", "20"))
-
-# External URLs
-CRTSH_URL = "https://crt.sh/?q=%25{q}%25&output=json"
-IP_API_URL = "http://ip-api.com/json/{ip}"
-DUCKDUCKGO_HTML = "https://html.duckduckgo.com/html/"
+SHODAN_API_KEY = "your_shodan_key"
+VIRUSTOTAL_API_KEY = "your_vt_key"
+IPINFO_TOKEN = "your_ipinfo_token"
